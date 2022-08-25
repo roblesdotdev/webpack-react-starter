1ec434fb612f13dab6d56f606004e0a9a7838fa5
roblesdotdev <httpsnip@gmail.com>
2022-08-24 21:26:37 -0300

    setup webpack dev mode

---
+++ webpack.config.dev.js
@@ -0,0 +1,47 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const buildDirectory = "dist";
+
+const outputDirectory = buildDirectory + "/app";
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+
+  output: {
+    path: path.join(__dirname, outputDirectory),
+
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".js", ".jsx"],
+        },
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  devServer: {
+    port: 3000,
+    hot: true,
+    open: true,
+  },
+
+  devtool: "inline-source-map",
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
